1201ed4525cad91ede78b032a57cbf09b9dd6969
Abhilash Patil <89824888+abhilash15500@users.noreply.github.com>
2025-02-15 14:06:03 +0530

    create a hashmap class

---
+++ hashmap.js
@@ -0,0 +1,9 @@
+class Hashmap {
+    constructor(loadFactor,capacity,bucket) {
+        this.loadFactor = loadFactor;
+        this.capacity = capacity;
+        this.bucket = new Array(capacity);
+    };
+
+    
+}
