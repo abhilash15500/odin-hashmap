00205b0780c17b33d57f95773eee6f7fa551baa9
Abhilash Patil <89824888+abhilash15500@users.noreply.github.com>
2025-02-15 14:07:09 +0530

    implement hash method on hashmap class

--- hashmap.js
+++ hashmap.js
@@ -5,5 +5,14 @@ class Hashmap {
         this.bucket = new Array(capacity);
     };
 
+    hash(key) {
+        let hashCode = 0;
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+          hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+        }
+        return hashCode;
+      };
     
-}
+    
+};
