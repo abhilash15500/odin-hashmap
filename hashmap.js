f22ef8356580ec9110034589cd3bc5a1ab022320
Abhilash Patil <89824888+abhilash15500@users.noreply.github.com>
2025-02-18 23:01:25 +0530

    fix bugs in length and remove methods

--- hashmap.js
+++ hashmap.js
@@ -5,7 +5,7 @@ class Hashmap {
     this.bucket = new Array(capacity);
   }
 
-  hash(key,capacity = this.capacity) {
+  hash(key, capacity = this.capacity) {
     let hashCode = 0;
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
@@ -16,19 +16,18 @@ class Hashmap {
   }
 
   set(key, value) {
-
-    if((this.length()+1) > (this.capacity * this.loadFactor)) {
-        let newHashMap = new Hashmap(0.8,(this.capacity*2));
+    if (!this.has(key)) {
+      if (this.length() + 1 > this.capacity * this.loadFactor) {
+        let newHashMap = new Hashmap(0.8, this.capacity * 2);
         let arrayOfKeyValues = this.entries();
-        arrayOfKeyValues.forEach(element => {
-            newHashMap.set(element[0],element[1]);
+        arrayOfKeyValues.forEach((element) => {
+          newHashMap.set(element[0], element[1]);
         });
-
-        newHashMap.set(key,value);
+        newHashMap.set(key, value);
         this.bucket = newHashMap.bucket;
         this.capacity = newHashMap.capacity;
-    };
-
+      }
+    }
 
     const hashedIndex = this.hash(key);
 
@@ -60,193 +59,176 @@ class Hashmap {
     let tempVariable;
     let isElementFound = false;
 
-    if(this.bucket[hashedIndex] !== undefined) {
+    if (this.bucket[hashedIndex] !== undefined) {
       tempVariable = this.bucket[hashedIndex].head;
     }
-  
+
     while (isElementFound === false) {
-
-      if(tempVariable === undefined) {
+      if (tempVariable === undefined) {
         return null;
-      }
-
-      else if (tempVariable.data[0] === key) {
+      } else if (tempVariable.data[0] === key) {
         isElementFound = true;
         return tempVariable.data[1];
-      }
-
-      else if(tempVariable.data[0] !== key && tempVariable.nextNode === null){
+      } else if (
+        tempVariable.data[0] !== key &&
+        tempVariable.nextNode === null
+      ) {
         return null;
       }
       tempVariable = tempVariable.nextNode;
     }
-  };
+  }
 
-
-  
   has(key) {
     const hashedIndex = this.hash(key);
     let tempVariable;
     let isElementFound = false;
 
-    if(this.bucket[hashedIndex] !== undefined) {
+    if (this.bucket[hashedIndex] !== undefined) {
       tempVariable = this.bucket[hashedIndex].head;
     }
-  
+
     while (isElementFound === false) {
-
-      if(tempVariable === undefined) {
+      if (tempVariable === undefined) {
         return false;
-      }
-
-      else if (tempVariable.data[0] === key) {
+      } else if (tempVariable.data[0] === key) {
         isElementFound = true;
         return true;
-      }
-
-      else if(tempVariable.data[0] !== key && tempVariable.nextNode === null){
+      } else if (
+        tempVariable.data[0] !== key &&
+        tempVariable.nextNode === null
+      ) {
         return false;
       }
       tempVariable = tempVariable.nextNode;
     }
-  };
+  }
 
   remove(key) {
     const hashedIndex = this.hash(key);
     let tempVariable;
     let isElementFound = false;
 
-    if(this.bucket[hashedIndex] !== undefined) {
+    if (this.bucket[hashedIndex] !== undefined) {
       tempVariable = this.bucket[hashedIndex].head;
     }
-  
+
     while (isElementFound === false) {
-      if(tempVariable === undefined) {
+      if (tempVariable === undefined) {
         return false;
-      }
-      else if (tempVariable.data[0] === key) {
+      } else if (tempVariable.data[0] === key) {
         tempVariable.data = undefined;
         isElementFound = true;
         return true;
-      }
-     else if(tempVariable.data[0] !== key && tempVariable.nextNode === null){
+      } else if (
+        tempVariable.data[0] !== key &&
+        tempVariable.nextNode === null
+      ) {
         return false;
       }
       tempVariable = tempVariable.nextNode;
     }
   }
 
-
   length() {
     let elementCount = 0;
     const bucket = this.bucket;
-    const arrayOfLinkedLists = bucket.filter((elements) =>  elements !== undefined);
-  
-    arrayOfLinkedLists.forEach(linkedList => {
+    const arrayOfLinkedLists = bucket.filter(
+      (elements) => elements !== undefined
+    );
+
+    arrayOfLinkedLists.forEach((linkedList) => {
       let tempVariable = linkedList.head;
       let isLinkedListIterated = false;
-      while(!isLinkedListIterated) {
-            if(tempVariable.nextNode === null) {
-              elementCount = elementCount + 1;
-              isLinkedListIterated = true;
-            } 
-            else if(tempVariable.data !== undefined) {
-              elementCount = elementCount + 1;
-            }
+      while (!isLinkedListIterated) {
+        if (tempVariable.nextNode === null) {
+          if(tempVariable.data !== undefined) {
+            elementCount = elementCount + 1;
+          }
+           isLinkedListIterated = true;
+        } else if (tempVariable.data !== undefined) {
+          elementCount = elementCount + 1;
+        }
 
         tempVariable = tempVariable.nextNode;
       }
-      
     });
     return elementCount;
-  };
+  }
 
   clear(capacity = this.capacity) {
     this.bucket = new Array(capacity);
-  };
-
-
-  keys() {
-    
-    const bucket = this.bucket;
-    const arrayOfLinkedLists = bucket.filter((elements) => typeof elements !== undefined);
-    const arrayOfKey = [];
-    arrayOfLinkedLists.forEach(linkedList => {
-      let tempVariable = linkedList.head;
-      let isLinkedListIterated = false;
-      while(!isLinkedListIterated) {
-            if(tempVariable.nextNode === null) {
-              arrayOfKey.push(tempVariable.data[0]);
-              isLinkedListIterated = true;
-            } 
-            else if(tempVariable.data !== undefined) {
-                  arrayOfKey.push(tempVariable.data[0]);
-            }
-
-        tempVariable = tempVariable.nextNode;
-      }
-      
-    });
-    return arrayOfKey;
-  };
-
-
-
-  values() {
-    
-    const bucket = this.bucket;
-    const arrayOfLinkedLists = bucket.filter((elements) => typeof elements !== undefined);
-    const arrayOfValues = [];
-    arrayOfLinkedLists.forEach(linkedList => {
-      let tempVariable = linkedList.head;
-      let isLinkedListIterated = false;
-      while(!isLinkedListIterated) {
-            if(tempVariable.nextNode === null) {
-              arrayOfValues.push(tempVariable.data[1]);
-              isLinkedListIterated = true;
-            } 
-            else if(tempVariable.data !== undefined) {
-                  arrayOfValues.push(tempVariable.data[1]);
-            }
-
-        tempVariable = tempVariable.nextNode;
-      }
-      
-    });
-    return arrayOfValues;
-  };
-
-
-  entries()  {
-    
-    
-      const bucket = this.bucket;
-      const arrayOfLinkedLists = bucket.filter((elements) => typeof elements !== undefined);
-      const arrayOfKeyValues = [];
-      arrayOfLinkedLists.forEach(linkedList => {
-        let tempVariable = linkedList.head;
-        let isLinkedListIterated = false;
-        while(!isLinkedListIterated) {
-              if(tempVariable.nextNode === null) {
-                arrayOfKeyValues.push([tempVariable.data[0],tempVariable.data[1]]);
-                isLinkedListIterated = true;
-              } 
-              else if(tempVariable.data !== undefined) {
-                arrayOfKeyValues.push([tempVariable.data[0],tempVariable.data[1]]);
-              }
-  
-          tempVariable = tempVariable.nextNode;
-        }
-        
-      });
-      return arrayOfKeyValues;
-
   }
 
+  keys() {
+    const bucket = this.bucket;
+    const arrayOfLinkedLists = bucket.filter(
+      (elements) => typeof elements !== undefined
+    );
+    const arrayOfKey = [];
+    arrayOfLinkedLists.forEach((linkedList) => {
+      let tempVariable = linkedList.head;
+      let isLinkedListIterated = false;
+      while (!isLinkedListIterated) {
+        if (tempVariable.nextNode === null) {
+          arrayOfKey.push(tempVariable.data[0]);
+          isLinkedListIterated = true;
+        } else if (tempVariable.data !== undefined) {
+          arrayOfKey.push(tempVariable.data[0]);
+        }
 
+        tempVariable = tempVariable.nextNode;
+      }
+    });
+    return arrayOfKey;
+  }
 
+  values() {
+    const bucket = this.bucket;
+    const arrayOfLinkedLists = bucket.filter(
+      (elements) => typeof elements !== undefined
+    );
+    const arrayOfValues = [];
+    arrayOfLinkedLists.forEach((linkedList) => {
+      let tempVariable = linkedList.head;
+      let isLinkedListIterated = false;
+      while (!isLinkedListIterated) {
+        if (tempVariable.nextNode === null) {
+          arrayOfValues.push(tempVariable.data[1]);
+          isLinkedListIterated = true;
+        } else if (tempVariable.data !== undefined) {
+          arrayOfValues.push(tempVariable.data[1]);
+        }
 
-};
+        tempVariable = tempVariable.nextNode;
+      }
+    });
+    return arrayOfValues;
+  }
+
+  entries() {
+    const bucket = this.bucket;
+    const arrayOfLinkedLists = bucket.filter(
+      (elements) => typeof elements !== undefined
+    );
+    const arrayOfKeyValues = [];
+    arrayOfLinkedLists.forEach((linkedList) => {
+      let tempVariable = linkedList.head;
+      let isLinkedListIterated = false;
+      while (!isLinkedListIterated) {
+        if (tempVariable.nextNode === null) {
+          arrayOfKeyValues.push([tempVariable.data[0], tempVariable.data[1]]);
+          isLinkedListIterated = true;
+        } else if (tempVariable.data !== undefined) {
+          arrayOfKeyValues.push([tempVariable.data[0], tempVariable.data[1]]);
+        }
+
+        tempVariable = tempVariable.nextNode;
+      }
+    });
+    return arrayOfKeyValues;
+  }
+}
 
 class LinkedList {
   constructor(head = null) {
@@ -263,59 +245,30 @@ class Node {
 
 // *-------------- initialize app ----------------* //
 
-let myHashmap = new Hashmap(0.8, 16);
-// Test 1: Basic Set and Get Functionality
-console.log(myHashmap.get("key1"));  // Expected: "value1"
+let test = new Hashmap(0.8, 16);
 
-// Test 2: Collisions (Insert and Retrieve Multiple Keys)
-myHashmap.set("key2", "value2");
-myHashmap.set("key3", "value3");
-console.log(myHashmap.get("key2"));  // Expected: "value2"
-console.log(myHashmap.get("key3"));  // Expected: "value3"
+// testing
+test.set('apple', 'red')
+test.set('apple', 'redish')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('carrot', 'orangeish')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('frog', 'greenish')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('ice cream', 'whitish')
+test.set('jacket', 'blue')
+test.set('jacket', 'bluish')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+test.set('moon', 'silver')
+test.set('moon', 'silverish')
 
-// Test 3: Dynamic Resizing (Inserting enough keys to exceed load factor)
-myHashmap.set("key4", "value4");
-myHashmap.set("key5", "value5");
-console.log("Capacity after resizing:", myHashmap.capacity);  // Expected: 8 (assuming initial capacity was 4 and it doubled)
 
-// Test 4: Removing Elements
-console.log(myHashmap.remove("key3"));  // Expected: true
-console.log(myHashmap.get("key3"));  // Expected: null
 
-// Test 5: Edge Case - Removing a non-existent key
-console.log(myHashmap.remove("nonexistent"));  // Expected: false
-
-// Test 6: Checking size consistency
-console.log(myHashmap.length());  // Expected: 4 (after adding 5 and removing 1)
-
-// Test 7: Keys Function
-console.log(myHashmap.keys());  // Expected: ["key1", "key2", "key4", "key5"]
-
-// Test 8: Values Function
-console.log(myHashmap.values());  // Expected: ["value1", "value2", "value4", "value5"]
-
-// Test 9: Entries Function
-console.log(myHashmap.entries());  // Expected: [["key1", "value1"], ["key2", "value2"], ["key4", "value4"], ["key5", "value5"]]
-
-// Test 10: Clear Function
-myHashmap.clear();
-console.log(myHashmap.length());  // Expected: 0
-console.log(myHashmap.keys());  // Expected: []
-
-// Test 11: Hashmap with empty key
-myHashmap.set("", "empty");
-console.log(myHashmap.get(""));  // Expected: "empty"
-
-// Test 12: Hashmap with numeric key (e.g., '123')
-myHashmap.set("123", "numeric");
-console.log(myHashmap.get("123"));  // Expected: "numeric"
-
-// Test 13: Hashmap with boolean key (e.g., true)
-myHashmap.set(true, "booleanTrue");
-console.log(myHashmap.get(true));  // Expected: "booleanTrue"
-
-// Test 14: Keys with special characters and long strings
-myHashmap.set("!@#$%^&*", "specialChars");
-myHashmap.set("a".repeat(100), "longString");  // Very long string key
-console.log(myHashmap.get("!@#$%^&*"));  // Expected: "specialChars"
-console.log(myHashmap.get("a".repeat(100)));  // Expected: "longString"
+console.log(test);
+  
